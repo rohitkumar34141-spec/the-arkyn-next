afffba7869ce88affd854dd3571d0f1edc8823c8
Rohit Zaykar <rohitkumar34141@gmail.com>
2025-12-30 03:55:08 -0800

    Add favicon logo

---
+++ pages/_document.js
@@ -0,0 +1,21 @@
+// pages/_document.js
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        {/* Favicon */}
+        <link rel="icon" href="/favicon.png" />
+
+        {/* Recommended extras */}
+        <link rel="apple-touch-icon" href="/favicon.png" />
+        <meta name="theme-color" content="#000000" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
